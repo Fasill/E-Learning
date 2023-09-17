715144780b872209d95b531cacfa5a5c9503226b
Fasill <fasilhawultie19@gmail.com>
2023-09-17 20:02:57 +0300

    hero section added with search bar

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,6 +4,7 @@ import back1 from "../../assets/images/back1.svg"
 import back2 from "../../assets/images/back2.svg"
 import back3 from "../../assets/images/back3.svg"
 import girl from  "../../assets/images/girl.svg"
+import search from "../../assets/images/search.svg" 
 
 export const Home = ()=>{
   return(
@@ -15,6 +16,18 @@ export const Home = ()=>{
           <img className = {style.backimg2} src = {back2}/>
           <img className = {style.backimg3} src = {back3}/>
           <img className = {style.girl} src = {girl}/>
+
+        </div>
+        <div className = {style.tittles}>
+          <div className = {style.search}>
+            <input type = "text" placeholder={`Search for courses `} />
+            {/* <img src = {search} />  */}
+
+          </div>
+          <div className={style.heroTittle} >
+            <h1>Explore what professionals like you are  learning the most</h1>
+          </div>
+          <button>Visit Courses</button>
 
         </div>
 

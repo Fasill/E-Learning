a67632147838a132825cdb271a66092aa9caca9a
Fasill <fasilhawultie19@gmail.com>
2023-09-17 17:45:52 +0300

    background images added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,32 @@
 import style from "./navbar.module.css"
-import img1 from  '../assets/images/logo.svg'
+import img1 from  '../assets/images/cart.svg'
 import img2 from  '../assets/images/list.svg'
 
 export const Navbar = ()=>{
   return(
     <div className={style.allNav}>
         
-      <div className={style.logo} ><img src= {img1}/><h1>E-Learn</h1></div>
+      <div className={style.logo} ><h1>CourseMania</h1></div>
 
       <ul className={style.allUl}>
         <li className={style.firstLi} ><a hrf = "/">Home</a></li>
-        <li className={style.firstLi} ><a hrf = "/">Features</a></li>
-        <li className={style.firstLi} ><a hrf = "/">How It Works</a></li>
-        <li className={style.firstLi} ><a hrf = "/">About Us</a></li>
-        <li className={style.firstLi} ><a hrf = "/">Testimponial</a></li>
-        <li className={style.firstLi} ><a hrf = "/">Blog</a></li>
+        <li className={style.firstLi} ><a hrf = "/">About</a></li>
+        <li className={style.firstLi} ><a hrf = "/">Courses</a></li>
+        <li className={style.firstLi} ><a hrf = "/">Tutors</a></li>
+        <li className={style.firstLi} ><a hrf = "/">Contact</a></li>
 
       
       </ul>
-      <button>Get the App</button>
+
+      <div className = {style.loginNregister}>
+        <a href = "/">Login</a>
+        <button>Register</button>
+        <div className = {style.cart}>
+          <p className =  {style.cartVal}>1</p>
+          <img src = {img1}/>  
+        </div>
+        
+      </div>
       <div className={style.list}>
         <img src={img2}/>
       </div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,25 @@
 import { Navbar } from "../../components/navbar"
+import style from "./style.module.css"
+import back1 from "../../assets/images/back1.svg"
+import back2 from "../../assets/images/back2.svg"
+import back3 from "../../assets/images/back3.svg"
+import girl from  "../../assets/images/girl.svg"
 
 export const Home = ()=>{
   return(
     <div>
       <Navbar/>
+      <div className = {style.hero}>
+        <div className = {style.background}>
+          <img className = {style.backimg1} src = {back1}/>
+          <img className = {style.backimg2} src = {back2}/>
+          <img className = {style.backimg3} src = {back3}/>
+          <img className = {style.girl} src = {girl}/>
+
+        </div>
+
+
+      </div>
       
     </div>
   )
